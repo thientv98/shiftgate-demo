7f0c7f3a57f6346c6fc343b611628a37381f40b0
Trương Văn Thiên <vanthien989@gmail.com>
2020-05-05 17:17:02 +0700

    add chat box admin

---
+++ shiftgate_admin/pages/chat/_id.vue
@@ -0,0 +1,350 @@
+<template>
+    <v-container fluid class="pt-0">
+        <v-row no-gutters class="wrap-breadcrumbs">
+            <span class="title">Chat</span>
+            <v-spacer></v-spacer>
+            <v-breadcrumbs :items="breadcrumbs" divider=">"></v-breadcrumbs>
+        </v-row>
+
+        <v-card class="mt-5 form-data wrapper-content" flat tile="">
+
+            <v-card-actions class="pa-4">
+                <v-subheader class="font-weight-bold">Chat</v-subheader>
+            </v-card-actions>
+
+            <v-divider></v-divider>
+
+            <v-card-text>
+
+                <v-row>
+                    <v-col cols="12">
+                        <div class="settings-tray">
+                            <div class="friend-drawer no-gutters friend-drawer--grey">
+                            <img class="profile-image" src="https://covidinspection.com/wp-content/uploads/2020/03/868320_people_512x512.png" alt="">
+                            <div class="text">
+                            <h6>Customer 1</h6>
+                            <p class="text-muted">Layin' down the law since like before Christ...</p>
+                            </div>
+                            <!-- <span class="settings-tray--right">
+                            <i class="material-icons">cached</i>
+                            <i class="material-icons">message</i>
+                            <i class="material-icons">menu</i>
+                            </span> -->
+                        </div>
+                        </div>
+                        <div class="chat-panel">
+                        <div class="chat-history">
+                            <div class="row no-gutters" v-for="(item, index) in chat" :key="index">
+                                <div class="col-md-3" :class="item.type == 'right' ? 'offset-md-9' : ''">
+                                    <div class="chat-bubble" :class="item.type == 'left' ? 'chat-bubble--left' : 'chat-bubble--right'" :data-time="item.time">
+                                        {{item.message}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 pb-0">
+                            <div class="chat-box-tray">
+                                <form @submit.prevent="sendMessage()" autocomplete="off">
+                                <!-- <i class="material-icons">sentiment_very_satisfied</i> -->
+                                <input id="chat-input" type="text" placeholder="Type your message here..." v-model="message" autocomplete="off">
+                                <!-- <i class="material-icons">mic</i> -->
+                                <button type="submit" :disabled="message == ''"><i class="material-icons">send</i></button>
+                                </form>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </v-col>
+                </v-row>
+
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+        </v-card>
+
+    </v-container>
+</template>
+
+<script>
+import JQuery from 'jquery'
+let $ = JQuery
+
+export default {
+    data() {
+        return {
+            message: '',
+            chat: [
+                {
+                    type: 'left',
+                    message: 'abc',
+                    time: "2020-5-5 17:10"
+                },
+                {
+                    type: 'right',
+                    message: 'bcd',
+                    time: "2020-5-5 17:10"
+                }
+            ],
+            breadcrumbs: [
+                {
+                    text: 'Home',
+                    disabled: false,
+                    href: '/',
+                },
+                {
+                    text: 'chat',
+                    href: '/chat',
+                },
+                {
+                    text: '編集',
+                    href: '',
+                }
+            ],
+        };
+    },
+
+    methods: {
+        sendMessage(){
+            let today = new Date()
+            let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+            let time = today.getHours() + ":" + today.getMinutes()
+            let dateTime = date+' '+time
+            this.chat.push({
+                type: 'right',
+                message: this.message,
+                time: dateTime
+            })
+            this.message = ''
+            this.scrollBottom()
+        },
+        scrollBottom(){
+            $(".chat-history").stop().animate({ scrollTop: $(".chat-history")[0].scrollHeight}, 1000);
+        }
+    },
+} 
+</script>
+
+<style scoped lang="scss">
+$blue: #74b9ff;
+
+
+
+.profile-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 40px;
+}
+
+.settings-tray {
+  background: #eee;
+  padding: 10px 15px;
+  
+  .no-gutters {
+    padding: 0;
+  }
+  
+  &--right {
+    float: right;
+    
+    i {
+      margin-top: 10px;
+      font-size: 25px;
+      color: grey;
+      margin-left: 14px;
+      transition: .3s;
+      
+      &:hover {
+        color: $blue;
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+.search-box {
+  background: #fafafa;
+  padding: 10px 13px;
+  
+  .input-wrapper {
+    background: #fff;
+    border-radius: 40px;
+    
+    i {
+      color: grey;
+      margin-left: 7px; 
+      vertical-align: middle;
+    }
+  }
+}
+
+input {
+  border: none;
+  border-radius: 30px;
+  width: calc(100% - 65px);
+
+  &::placeholder {
+    color: #e3e3e3;
+    font-weight: 300;
+    margin-left: 20px;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+
+.friend-drawer {
+  padding: 10px 15px;
+  display: flex;
+  vertical-align: baseline;
+  background: #fff;
+  transition: .3s ease;
+  
+  &--grey {
+    background: #eee;
+  }
+  
+  .text {
+    margin-left: 12px;
+    width: 70%;
+    
+    h6 {
+      margin-top: 6px;
+      margin-bottom: 0;
+      font-size: 20px;
+    }
+    
+    p {
+      margin: 0;
+    }
+  }
+  
+  .time {
+    color: grey;
+  }
+  
+  &--onhover:hover {
+    background: $blue;
+    cursor: pointer;
+    
+    p,
+    h6,
+    .time {
+      color: #fff !important;
+    }
+  }
+}
+
+.chat-bubble {
+  padding: 10px 14px;
+  background: #eee;
+  margin: 10px 30px;
+  border-radius: 9px;
+  position: relative;
+  animation: fadeIn 1s ease-in;
+  
+  &:after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-bottom: 0;
+    margin-top: -10px;
+  }
+  
+  &--left {
+     &:after {
+      left: 0;
+      border-right-color: #eee;
+	    border-left: 0;
+      margin-left: -20px;
+    }
+  }
+  
+  &--right {
+    &:after {
+      right: 0;
+      border-left-color: $blue;
+	    border-right: 0;
+      margin-right: -20px;
+    }
+  }
+}
+
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+
+
+.offset-md-9 {
+  .chat-bubble {
+    background: $blue;
+    color: #fff;
+  }
+}
+
+.chat-box-tray {
+  background: #eee;
+  display: flex;
+  align-items: baseline;
+  padding: 10px 15px;
+  align-items: center;
+  margin-top: 19px;
+  bottom: 0;
+    form{
+        width: 100%;
+    }
+    input {
+        margin: 0 10px;
+        padding: 6px 15px;
+        background-color: white;
+        &::placeholder{
+            color: gray;
+        }
+    }
+  
+    i {
+        color: #74b9ff;
+        font-size: 30px;
+        vertical-align: middle;
+        
+        
+    }
+}
+.chat-panel{
+    border: 1px solid #eeeeee;
+    border-top: 0px;
+    border-bottom: 0px;
+}
+.chat-bubble.chat-bubble--left::before {
+    content: attr(data-time);
+    position: absolute;
+    left: 0px;
+    bottom: -20px;
+    display: block;
+    font-size: .750rem;
+    color: rgba(84, 110, 122,1.0);
+}
+.chat-bubble.chat-bubble--right::before {
+    content: attr(data-time);
+    position: absolute;
+    right: 0px;
+    bottom: -20px;
+    display: block;
+    font-size: .750rem;
+    color: rgba(84, 110, 122,1.0);
+}
+.chat-history{
+    height: 300px;
+    overflow: hidden auto;
+}
+</style>
--- shiftgate_front/components/common/Chat.vue
+++ shiftgate_front/components/common/Chat.vue
@@ -1,47 +1,42 @@
 <template>
-<div @blur="hideChatBox()"> 
+    <div v-if="this.$store.state.auth.loggedIn"> 
   
-    <div id="chat-circle" class="btn btn-raised" @click="showChatBox()">
-        <div id="chat-overlay"></div>
-		    <!-- <i class="material-icons">speaker_phone</i> -->
-        <i class="far fa-comment" style="font-size:25px"></i>
-	</div>
-  
-  <div class="chat-box">
-    <div class="chat-box-header">
-      ChatBot
-      <span class="chat-box-toggle" @click="hideChatBox()"><i class="material-icons">close</i></span>
-    </div>
-    <div class="chat-box-body">
-        <div class="chat-box-overlay">   
+        <div id="chat-circle" class="btn btn-raised" @click="showChatBox()">
+            <div id="chat-overlay"></div>
+                <!-- <i class="material-icons">speaker_phone</i> -->
+            <i class="far fa-comment" style="font-size:25px"></i>
         </div>
-        <div class="chat-logs">
-            <div class="chat-msg user">
-                <span class="msg-avatar">
-                    <img src="https://covidinspection.com/wp-content/uploads/2020/03/868320_people_512x512.png">
-                </span>
-                <div class="cm-msg-text" data-time="12:30">Contact to shiftgate!</div>
+  
+        <div class="chat-box">
+            <div class="chat-box-header">
+            ChatBot
+            <span class="chat-box-toggle" @click="hideChatBox()"><i class="material-icons">close</i></span>
             </div>
-            <div class="chat-msg" :class="item.type" v-for="(item, index) in chat" :key="index">
-                <span class="msg-avatar">
-                    <img src="https://covidinspection.com/wp-content/uploads/2020/03/868320_people_512x512.png">
-                </span>
-                <div class="cm-msg-text" :data-time="item.time">{{item.message}}</div>   
+            <div class="chat-box-body">
+                <div class="chat-box-overlay">   
+                </div>
+                <div class="chat-logs">
+                    <div class="chat-msg user">
+                        <span class="msg-avatar">
+                            <img src="https://covidinspection.com/wp-content/uploads/2020/03/868320_people_512x512.png">
+                        </span>
+                        <div class="cm-msg-text" data-time="2020-5-5 17:10">Contact to shiftgate!</div>
+                    </div>
+                    <div class="chat-msg" :class="item.type" v-for="(item, index) in chat" :key="index">
+                        <span class="msg-avatar">
+                            <img src="https://covidinspection.com/wp-content/uploads/2020/03/868320_people_512x512.png">
+                        </span>
+                        <div class="cm-msg-text" :data-time="item.time">{{item.message}}</div>   
+                    </div>
+                </div><!--chat-log -->
             </div>
-        </div><!--chat-log -->
-    </div>
-    <div class="chat-input">
-        <form @submit.prevent="sendMessage()" autocomplete="off">  
-        <input type="text" id="chat-input" placeholder="Send a message..." v-model="message" autocomplete="off"/>
-        <button type="submit" class="chat-submit" id="chat-submit" :disabled="message == ''"><i class="material-icons">send</i></button>  
-        </form> 
-    </div>
-  </div>
-  
-  
-  
-  
-</div>
+            <div class="chat-input">
+                <form @submit.prevent="sendMessage()" autocomplete="off">  
+                <input type="text" id="chat-input" placeholder="Send a message..." v-model="message" autocomplete="off"/>
+                <button type="submit" class="chat-submit" id="chat-submit" :disabled="message == ''"><i class="material-icons">send</i></button>  
+                </form> 
+            </div>
+        </div>
     </div>
 </template>
 
@@ -56,8 +51,8 @@ export default {
             chat: [
                 {
                     type: 'self',
-                    message: 'aamama mamamam',
-                    time: "16:30"
+                    message: 'abc',
+                    time: "2020-5-5 17:10"
                 }
             ]
         }
@@ -74,9 +69,14 @@ export default {
             $(".chat-box").toggle('scale') 
         },
         sendMessage(){
+            let today = new Date()
+            let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+            let time = today.getHours() + ":" + today.getMinutes()
+            let dateTime = date+' '+time
             this.chat.push({
                 type: 'self',
-                message: this.message
+                message: this.message,
+                time: dateTime
             })
             this.message = ''
             this.scrollBottom()
